80f8be1f210b0d887603e5027c4b281e23f7289d
Palina Karabovich <polina.korobovich@yandex.by>
2022-04-13 16:43:12 +0200

    1 review fixes

--- script/index.js
+++ script/index.js
@@ -1,22 +1,18 @@
 let page = document.querySelector('.page'); 
 let formElement = page.querySelector('.popup__form'); 
-let nameInput = formElement.querySelector('.popup__input-name');
-let jobInput = formElement.querySelector('.popup__input-description');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_description');
 let nameOnPage = page.querySelector('.profile__info-name');
 let jobOnPage = page.querySelector('.profile__info-caption');
 let editButton = page.querySelector('.profile__info-edit-button');
 let closeButton = page.querySelector('.popup__close-button');
 let popup = page.querySelector('.popup');
 
-editButton.addEventListener('click',popupOpen);
-formElement.addEventListener('submit', formSubmitHandler); 
-closeButton.addEventListener('click', popupClose);
-
-function popupClose(){
+function closePopUp(){
     popup.classList.remove('popup_opened');
 }
 
-function popupOpen(){
+function openPopUp(){
     popup.classList.add('popup_opened');
     nameInput.value = nameOnPage.textContent;
     jobInput.value = jobOnPage.textContent;
@@ -24,11 +20,13 @@ function popupOpen(){
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let newName = nameInput.value;
-    let newJob = jobInput.value;
-    nameOnPage.textContent = newName;
-    jobOnPage.textContent = newJob;
-    popupClose();
+    nameOnPage.textContent = nameInput.value;
+    jobOnPage.textContent = jobInput.value;
+    closePopUp();
 }
+
+editButton.addEventListener('click',openPopUp);
+formElement.addEventListener('submit', formSubmitHandler); 
+closeButton.addEventListener('click', closePopUp);
 
 
